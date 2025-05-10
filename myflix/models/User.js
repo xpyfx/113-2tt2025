c8acc47f0a59003c0863c01da536d2475cc053ee
Anastasia <phoebe.pyf@gmail.com>
2025-05-10 15:05:06 +0800

    delete some js file i add

--- myflix/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
-  favorites: [String]  // 儲存影片 ID
-});
-
-module.exports = mongoose.model('User', userSchema);
